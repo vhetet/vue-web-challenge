9d04f2ca8f7ecfdc822ebb1be777419f4be1c332
vhetet <vhetet@solstice.com>
2020-09-09 21:32:37 -0500

    remove unused views and setup routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import ContactList from "../components/contact-list/index.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +8,18 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: ContactList
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/detail",
+    name: "Detail",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(
+        /* webpackChunkName: "about" */ "../components/contact-detail/index.vue"
+      )
   }
 ];
 
