24cf7955eb02568e3b071b4f17a8093c4910a159
vhetet <vhetet@solstice.com>
2020-09-09 22:01:58 -0500

    add contact card

---
+++ src/components/contact-card/index.vue
@@ -0,0 +1,4 @@
+<template src="./contact-card.html"></template>
+<script src="./contact-card.js"></script>
+<style src="./contact-card.scss" scoped lang="scss"></style>
+
--- src/components/contact-list/contact-list.js
+++ src/components/contact-list/contact-list.js
@@ -1,22 +1,23 @@
+import axios from "axios";
+
+import contactCard from "../contact-card/index.vue"
 
 export default {
-  name: 'contact-list',
-  components: {},
+  name: "contact-list",
+  components: {
+    "contact-card": contactCard
+  },
   props: [],
-  data () {
+  data() {
     return {
-
-    }
+      contacts: []
+    };
   },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-
+  mounted() {
+    axios
+      .get("https://s3.amazonaws.com/technical-challenge/v3/contacts.json")
+      .then(res => {
+        this.contacts = res.data;
+      });
   }
-}
-
-
+};
